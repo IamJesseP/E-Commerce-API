1f8e1d7129349c1e8ff0b5bbfd1b1cd03edc6a10
Jesse <jesseorhs@gmail.com>
2023-05-23 10:48:17 -0700

    feat: add user schema to models, config packages
    This commit adds the UserSchema mongoose model that takes in a name,
    email, password, and role. Also imported the morgan package for easier
    http request tracking. Lastly, imported and configured the
    eslint/prettier package.

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
+
 // express
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
+
+// misc. packages
+const morgan = require('morgan');
 
 // database
-const connectDB = require('./db/connect')
+const connectDB = require('./db/connect');
 
 // middleware
-const notFoundMiddleWare = require('./middleware/not-found')
-const errorHandlerMiddleWare = require('./middleware/error-handler')
+const notFoundMiddleWare = require('./middleware/not-found');
+const errorHandlerMiddleWare = require('./middleware/error-handler');
 
-
-app.use(express.json) //<- parse req.body
+app.use(morgan('tiny'));
+app.use(express.json()); // <- parse req.body
 
 // homepage route
-app.get('/', (req,res) =>{
-    res.send('e-commerce api')
-})
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
 
-app.use(notFoundMiddleWare)
-app.use(errorHandlerMiddleWare)
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleWare);
+// Middleware functions are executed in the order in which they are defined, and the first middleware
+// function that terminates the request-response cycle (by calling res.send, res.end, or res.json, for example)
+// will prevent any subsequent middleware functions from being executed.
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
