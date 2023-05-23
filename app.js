52bc945e8015bd5d351f6e55d51ee4e34444ad11
Jesse <jesseorhs@gmail.com>
2023-05-22 20:07:56 -0700

    feat: basic routes and middleware
    Setup '/' GET Route, express.json() middleware, 404 and errorHandler
    middleware, import 'express-async-errors' package

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+require('express-async-errors')
 // express
 const express = require('express')
 const app = express()
@@ -7,6 +7,20 @@ const app = express()
 // database
 const connectDB = require('./db/connect')
 
+// middleware
+const notFoundMiddleWare = require('./middleware/not-found')
+const errorHandlerMiddleWare = require('./middleware/error-handler')
+
+
+app.use(express.json) //<- parse req.body
+
+// homepage route
+app.get('/', (req,res) =>{
+    res.send('e-commerce api')
+})
+
+app.use(notFoundMiddleWare)
+app.use(errorHandlerMiddleWare)
 
 const port = process.env.PORT || 5000
 const start = async () => {
