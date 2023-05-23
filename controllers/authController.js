337e0f66712f2b7aedf215053dcbd09d7bb8c36c
Jesse <jesseorhs@gmail.com>
2023-05-23 12:24:14 -0700

    feat: add register user functionality
    This commit adds register functionality by using the User.create()
    method. The register controller also handles unique emails and only
    allows the user enum to be used.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,18 @@
+/* eslint-disable no-unused-vars */
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const { email, name, password } = req.body;
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    throw new CustomError.BadRequestError(
+      'Email is already reigstered to another user',
+    );
+  }
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send('login user');
