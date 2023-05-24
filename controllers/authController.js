7765a9501ace08cda5d0ce5b1ee9da0f44d9fbeb
Jesse <jesseorhs@gmail.com>
2023-05-23 18:37:57 -0700

    fix: remove duplicate logic for unique emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,11 +8,7 @@ const { createJWT } = require('../utils/index');
 const register = async (req, res) => {
   const { email, name, password } = req.body;
   const emailExists = await User.findOne({ email });
-  if (emailExists) {
-    throw new CustomError.BadRequestError(
-      'Email is already reigstered to another user',
-    );
-  }
+
   const user = await User.create({ name, email, password });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
   const token = createJWT({ payload: tokenUser });
