e887df9242d3a780b51d4c0b4d7bb70bbb6b1282
Jesse <jesseorhs@gmail.com>
2023-05-24 10:43:34 -0700

    feat: add login functionality
    This commit adds the login functionality to authController.js. The login
    checks for valid emails/passwords and passes the token in cookies if all
    checks are passed. An error is thrown to the user otherwise.

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,13 +8,30 @@ const { attachCookiesToRes } = require('../utils/index');
 const register = async (req, res) => {
   const { email, name, password } = req.body;
   const user = await User.create({ name, email, password });
+
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
   attachCookiesToRes({ res, user: tokenUser });
-
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid email or password');
+  }
+
+  const validPassword = user.comparePassword(password);
+  if (!validPassword) {
+    throw new CustomError.UnauthenticatedError('Invalid email or password');
+  }
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToRes({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
   res.send('logout user');
