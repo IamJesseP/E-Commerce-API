afdef32fba5eb9f83080a32932e6624a5411db9a
Jesse <jesseorhs@gmail.com>
2023-05-23 18:32:21 -0700

    feat: add jwt functionality to register user
    This commit creates a utilities folder which contains the logic for
    creating json web tokens using jwt.sign(). Token validation is also
    within jwt.js using jwt.verify().

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const User = require('../models/User');
+const { createJWT } = require('../utils/index');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -12,7 +14,9 @@ const register = async (req, res) => {
     );
   }
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send('login user');
