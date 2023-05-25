f2406be268cbd634e04520ac634c69efe88aafe6
Jesse <jesseorhs@gmail.com>
2023-05-24 20:31:05 -0700

    feat: add authentication to .get routes
    This commit adds authentication middle functionality. The middleware is
    used within the getUser(s) routes.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const auth = require('../controllers/authController');
 const user = require('../controllers/userController');
+const { authenticateUser } = require('../middleware/authentication');
 
 const router = express.Router();
 
-router.route('/').get(user.getAllUsers);
+router.route('/').get(authenticateUser, user.getAllUsers);
 router.route('/showMe').get(user.showCurrentUser);
 router.route('/updateUser').patch(user.updateUser);
 router.route('/updateUserPassword').patch(user.updateUserPassword);
 
-router.route('/:id').get(user.getSingleUser);
+router.route('/:id').get(authenticateUser, user.getSingleUser);
 
 module.exports = router;
